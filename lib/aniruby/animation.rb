703a7620eee82bc2c49af32b81e78c6e6517a66a
Chadowo <83732118+Chadowo@users.noreply.github.com>
2023-08-20 21:27:28 -0300

    Document with YARD and use new `Frames` object

--- lib/aniruby/animation.rb
+++ lib/aniruby/animation.rb
@@ -1,27 +1,46 @@
 
 module AniRuby
+  # Has a AniRuby::Frames colletion, with a simple counter to keep track of
+  # current frame plus looping and pausing functionality
   class Animation
-    attr_accessor :frames, :current_frame, :loop
+    # @return [AniRuby::Frames] The collection of frames this animation uses
+    attr_accessor :frames
+    # @return [Integer] The current frame index of the animation
+    attr_accessor :current_frame
+    # @return [Boolean] The loop status of the animation
+    attr_accessor :loop
 
-    def initialize(spritesheet, clip_w, clip_h, retro = false, loop = true, *durations)
-      @frames = []
-      @current_frame = 0
-
-      @frame_w = clip_w
-      @frame_h = clip_h
+    # Create a new animation
+    #
+    # @param spritesheet [String] Path to the spritesheet file
+    # @param frame_w [Integer] The width of each individual frame
+    # @param frame_h [Integer] The height of each individual frame
+    # @param retro [Boolean] If true, the animation will not be interpolated when scaled
+    # @param loop [Boolean] If true, the animation will loop indefinitely
+    # @param durations [Integer] The duration of the frames in milliseconds (500
+    #                             is half a second, 1000 a second, etc). If
+    #                             there's more than one duration, then they will
+    #                             be mapped to each frame of the animation, the
+    #                             frames with no specified duration will default to 100
+    #
+    # @return [Animation] A new animation ready to play
+    def initialize(spritesheet, frame_w, frame_h, retro = false, loop = true, *durations)
+      @frame_w = frame_w
+      @frame_h = frame_h
 
       @loop = loop
+
+      @current_frame = 0
       @pause = false
 
-      Gosu::Image.load_tiles(spritesheet,
-                             @frame_w,
-                             @frame_h,
-                             retro: retro).each_with_index do |sprite, idx|
-        @frames << AniRuby::Frame.new(sprite)
-      end
+      @frames = AniRuby::Frames.new(Gosu::Image.load_tiles(spritesheet,
+                                                           @frame_w,
+                                                           @frame_h,
+                                                           retro: retro))
 
+      # TODO: Maybe I could shorten this, adding an extra argument to
+      #       AniRuby::Frames
       if durations.one?
-        # Set every frame's delay to the only one provide
         @frames.each { |frame| frame.duration = durations[0]}
       else
         @frames.each_with_index do |frame, idx|
@@ -32,20 +51,39 @@ module AniRuby
       end
     end
 
+    # Get the width of the current frame's image
+    #
+    # @return [Integer]
     def width
       @frames[@current_frame].sprite.width
     end
 
+    # Get the height of the current frame's image
+    #
+    # @return [Integer]
     def height
       @frames[@current_frame].sprite.height
     end
 
+    # Update the animation, advancing the frame counter. Note that this won't do
+    # do anything if the animation is paused or has finished
     def update
       return if done? || paused?
 
       @current_frame += 1 if frame_expired?
     end
 
+    # Draw the animation
+    #
+    # @param x [Integer] The X coordinate
+    # @param y [Integer] The Y coordinate
+    # @param z [Integer] The Z order
+    # @param scale_x [Float] The horizontal scale factor
+    # @param scale_y [Float] The vertical scale factor
+    # @param color [Gosu::Color] The color to usw when drawing
+    # @param mode [:default, :additive] The blending mode
+    #
+    # (see {draw_rot})
     def draw(x, y, z = 0,
              scale_x = 1,
              scale_y = 1,
@@ -58,6 +96,20 @@ module AniRuby
       @current_frame = 0 if @loop && done?
     end
 
+    # Draw the animation rotated, with its rotational center at (x, y).
+    #
+    # @param x [Integer] The X coordinate
+    # @param y [Integer] The Y coordinate
+    # @param z [Integer] The Z order
+    # @param angle [Float] The angle. in degrees
+    # @param center_x [Float] the relative horizontal rotation origin
+    # @param center_y [Float] the relative vertical rotation origin
+    # @param scale_x [Float] The horizontal scale factor
+    # @param scale_y [Float] The vertical scale factor
+    # @param color [Gosu::Color] The color to usw when drawing
+    # @param mode [:default, :additive] The blending mode
+    #
+    # (see {draw})
     def draw_rot(x, y, z = 0,
                  angle = 0,
                  center_x = 0.5,
@@ -74,33 +126,57 @@ module AniRuby
       @current_frame = 0 if @loop && done?
     end
 
+    # Pause the animation
+    #
+    # (see {resume})
     def pause
       @pause = true
     end
 
+    # Resume the animation
+    #
+    # (see {pause})
     def resume
       @pause = false
     end
 
+    # Set the animation to the beginning frame
     def reset
       @current_frame = 0
     end
 
+    # Set the duration for all frames in the animation
+    #
+    # @param ms [Integer] The new duration in milliseconds
+    def duration(ms)
+      @frames.each { |frame| frame.duration = ms}
+    end
+
+    # Is the animation finished?
+    #
+    # @return [Boolean]
+    # @note This method will return true in intervals if the animation loops
     def done?
       true if @current_frame == @frames.size - 1
     end
 
+    # Is the animation paused?
+    #
+    # @return [Boolean]
     def paused?
       return true if @pause
 
       false
     end
 
-    # Returns the current frame object
+    # Get the current frame
+    #
+    # @return [AniRuby::Frame]
     def get_current_frame
       @frames[@current_frame % @frames.size]
     end
 
+    # Has the current frame's duration expired?
     def frame_expired?
       now = Gosu.milliseconds
       @last_frame ||= now
@@ -108,10 +184,6 @@ module AniRuby
       if (now - @last_frame) > @frames[@current_frame].duration
         @last_frame = now
       end
-    end
-
-    def duration(ms)
-      @frames.each { |frame| frame.duration = ms}
     end
   end
 end
