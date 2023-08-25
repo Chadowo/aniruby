5deb239466beaeef0f4474e8f5eccf0c0fd7bfbc
Chadowo <83732118+Chadowo@users.noreply.github.com>
2023-08-25 20:35:31 -0300

    Use a reporter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,11 @@
 
+require 'minitest/reporters'
+
 module TestHelper
   # Get the abosule path to an asset under media/
   def media_dir(asset)
     File.join(File.expand_path('..', __FILE__), 'media', asset)
   end
 end
+
+Minitest::Reporters.use!
