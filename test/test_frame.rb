c58fbd40a3f083807b7ef87614c16963e8922985
Chadowo <83732118+Chadowo@users.noreply.github.com>
2023-09-08 20:35:43 -0300

    Remove one space of indentation from a comment, replace a comment that is unhelpful with one more sensible. Add another test for the dimensions (width, height) of a frame's image.

--- test/test_frame.rb
+++ test/test_frame.rb
@@ -29,17 +29,28 @@ class FrameTest < Minitest::Test
     end
   end
 
-   # We provide 300ms as duration for two frames
+  # We provided 300ms as duration for two frames
   def test_custom_duration
     @sample_animation.frames[0..1].each do |f|
       assert_equal 0.3, f.duration
     end
   end
 
-  # The default is 100ms
   def test_default_duration
+    # For the rest of the frames...
     @sample_animation.frames[2..3].each do |f|
       assert_equal 0.1, f.duration
+    end
+  end
+
+  def test_dimensions
+    @sample_animation.frames[2..3].each do |f|
+      assert_equal @sprite_w, f.width
+      assert_equal @sprite_h, f.height
+
+      # Test the aliases too
+      assert_equal @sprite_w, f.w
+      assert_equal @sprite_h, f.h
     end
   end
 end
