6302f32c8729b89bbeb61e9481a5e02c3309e091
Chadowo <83732118+Chadowo@users.noreply.github.com>
2023-09-08 20:46:28 -0300

    Use the more descriptive name 'frame' instead of f

--- test/test_frame.rb
+++ test/test_frame.rb
@@ -22,33 +22,33 @@ class FrameTest < Minitest::Test
 
   # The sprite of a frame is a simple Gosu::Image
   def test_sprite
-    @sample_animation.frames.each do |f|
-      assert_instance_of Gosu::Image, f.sprite
+    @sample_animation.frames.each do |frame|
+      assert_instance_of Gosu::Image, frame.sprite
     end
   end
 
   # We provided 300ms as duration for two frames
   def test_custom_duration
-    @sample_animation.frames[0..1].each do |f|
-      assert_equal 0.3, f.duration
+    @sample_animation.frames[0..1].each do |frame|
+      assert_equal 0.3, frame.duration
     end
   end
 
   def test_default_duration
     # For the rest of the frames...
-    @sample_animation.frames[2..3].each do |f|
-      assert_equal 0.1, f.duration
+    @sample_animation.frames[2..3].each do |frame|
+      assert_equal 0.1, frame.duration
     end
   end
 
   def test_dimensions
-    @sample_animation.frames[2..3].each do |f|
-      assert_equal @sprite_w, f.width
-      assert_equal @sprite_h, f.height
+    @sample_animation.frames[2..3].each do |frame|
+      assert_equal @sprite_w, frame.width
+      assert_equal @sprite_h, frame.height
 
       # Test the aliases too
-      assert_equal @sprite_w, f.w
-      assert_equal @sprite_h, f.h
+      assert_equal @sprite_w, frame.w
+      assert_equal @sprite_h, frame.h
     end
   end
 end
