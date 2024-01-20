decf3f61f78fcd27ff920e4ad1946b795875ec37
Chadowo <83732118+Chadowo@users.noreply.github.com>
2024-01-20 19:44:46 -0300

    Remove the test file for the version, I don't know why I added it first place, it's not really useful

--- test/test_version.rb
+++
@@ -1,8 +0,0 @@
-require 'minitest/autorun'
-
-class TestVersion < Minitest::Test
-  def test_version
-    assert_equal "#{AniRuby::MAJOR}.#{AniRuby::MINOR}.#{AniRuby::PATCH}",
-                 AniRuby::VERSION
-  end
-end
