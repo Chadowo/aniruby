60bd4b31932a4d28e718060bfbc8774d045d7371
Chadowo <83732118+Chadowo@users.noreply.github.com>
2024-02-20 01:18:13 -0300

    Use `File.expand_path(__dir__)` instead of using `__FILE__`

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,7 @@ require_relative '../lib/aniruby'
 module TestHelper
   # Get the abosule path to an asset under media/
   def media_dir(asset)
-    File.join(File.expand_path('..', __FILE__), 'media', asset)
+    File.join(File.expand_path(__dir__), 'media', asset)
   end
 end
 
